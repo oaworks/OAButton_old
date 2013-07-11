b6c3a2368a1e64dfc662e011d9c8c4b33ff70b94
Alf Eaton <alf@hubmed.org>
2013-07-11 22:31:20 +0100

    Clean up the javascript

--- public/js/add.js
+++ public/js/add.js
@@ -1,5 +1,4 @@
-var accessed = document.getElementById('id_accessed');
-if (accessed) accessed.value = new Date();
+$('#id_accessed').val(new Date());
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -16,21 +15,30 @@ function showPosition(position) {
 
 getLocation();
 
-
-$(function() {
-  $('form').submit(function() {
-    // Do geocoding only if needed
-    if ($('#id_coords').val() === "") {
-      base_url = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=';
-      var location;
-      $.ajax({
-        url: base_url + encodeURIComponent($('input#id_location').val()),
-        async: false,
-        success: function(response) {
-          location = response.results[0].geometry.location;
-        }
-      });
+function geocode() {
+  return $.ajax({
+    url: 'http://maps.googleapis.com/maps/api/geocode/json',
+    data: {
+      sensor: 'false',
+      address: $('#id_location').val()
+    },
+    success: function(response) {
+      var location = response.results[0].geometry.location;
       $('#id_coords').val([location.lat, location.lng]);
     }
   });
-});
+}
+
+function onSubmit(event) {
+  var form = $(this);
+
+  // Do geocoding only if needed
+  if (!$('#id_coords').val()) {
+    event.preventDefault();
+    geocode().then(function() {
+      form.submit();
+    });
+  }
+};
+
+$('form').on("submit", onSubmit);
