oaworks/OAButton_old

commit c9160f8e9435da6a923db6b6ac0bd4cc1c63d6bb

Couple of tweaks from js lint

public/js/add.js
@@ -26,11 +26,11 @@ $(function() {
             db: "pmc",
             retmax: 1,
             term: doi + "[DOI]",
-            tool: "oabutton",
+            tool: "oabutton"
           },
           dataType: "xml",
           success: function(response) {
-            if (response.getElementsByTagName('Count')[0].textContent == 0) {
+            if (response.getElementsByTagName('Count')[0].textContent === 0) {
               return;
             }
 

commit 5b08a2c544cfda845ed29da170a807c100a105d2

Fix bug introduced by me in c9160f8e94 :\
Thanks, @hubgit!

public/js/add.js
@@ -30,7 +30,7 @@ $(function() {
           },
           dataType: "xml",
           success: function(response) {
-            if (response.getElementsByTagName('Count')[0].textContent === 0) {
+            if (response.getElementsByTagName('Count')[0].textContent === '0') {
               return;
             }
 