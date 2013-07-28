74cabfe41d0598f153a46cd69dacabdeda5869bd
Victor Ng <victor@crankycoder.com>
2013-07-28 14:48:17 -0400

    basic stories page renders and static content is serving.  yay!

--- oabutton/oabutton/settings.py
+++ oabutton/oabutton/settings.py
@@ -64,9 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/Users/victorng/dev/oabutton/python/oabutton/oabutton/static/public',
 )
 
 # List of finder classes that know how to find static files in
