f684da9dd3199a183af261b8d5dc62d1f0f5c903
Victor Ng <victor@crankycoder.com>
2013-08-14 22:54:23 -0400

    URL changes for single page layout

--- oabutton/oabutton/urls.py
+++ oabutton/oabutton/urls.py
@@ -5,20 +5,21 @@ from django.contrib import admin
 admin.autodiscover()
 
 from oabutton.apps.bookmarklet import views as bm
+from oabutton.apps.web import views as web
 
 urlpatterns = patterns('',
-    url(r'^$', bm.homepage, name="homepage"),
-    url(r'^about/$', bm.about, name="about"),
-    url(r'^stories/$', bm.show_stories, name="show_stories"),
-    url(r'^map/$', bm.show_map, name="show_map"),
+    url(r'^$', web.homepage, name="homepage"),
 
-    # TODO: this should really be pushed into a /api/ URL
-    # since we don't want to clutter up the top level URL namespace
-    url(r'^download.json$', bm.get_json, name="get_json"),
+    #url(r'^about/$', bm.about, name="about"),
+    #url(r'^stories/$', bm.show_stories, name="show_stories"),
+    #url(r'^map/$', bm.show_map, name="show_map"),
+
+    # this has been moved to /api
+    #url(r'^download.json$', bm.get_json, name="get_json"),
 
     # I just jammed this in here while i sort out all the URL
     # mappings.
-    url(r'^bookmarklet/', include('oabutton.apps.bookmarklet.urls')),
+    #url(r'^bookmarklet/', include('oabutton.apps.bookmarklet.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
