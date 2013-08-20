e898816839e6856bbf2e9f2bafa41cba2321ec11
Victor Ng <victor@crankycoder.com>
2013-08-19 23:20:16 -0400

    basic rendering of blocked papers works now against the sqlite database

--- oabutton/oabutton/apps/web/views.py
+++ oabutton/oabutton/apps/web/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, HttpResponseServerError
-from django.shortcuts import render
 from django.core import serializers
+from django.shortcuts import render_to_response
+from django.core import serializers
+
+from oabutton.apps.bookmarklet.models import Event
 
 try:
     from simplejson import dumps
@@ -9,4 +12,5 @@ except:
 
 
 def homepage(req):
-    return render(req, 'web/index.html')
+    data = serializers.serialize("json", Event.objects.all())
+    return render_to_response('web/index.html', {'events': data})
