12aa3780c98f84b987ca49f271b42edd1e608d0e
Victor Ng <victor@crankycoder.com>
2013-09-06 14:23:09 -0400

    added psycopg2 and repointed to postgresql

--- oabutton/settings.py
+++ oabutton/settings.py
@@ -22,7 +22,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'oabutton.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
