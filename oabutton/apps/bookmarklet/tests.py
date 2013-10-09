a4483c59d13cfecee878c70ed2ddd288cc15aaa0
Victor Ng <victor@crankycoder.com>
2013-10-08 23:46:06 -0400

    added test to verify that JSON serialization is compatible with mapping

--- oabutton/apps/bookmarklet/tests.py
+++ oabutton/apps/bookmarklet/tests.py
@@ -7,10 +7,11 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.test.client import Client
+from nose.tools import eq_, ok_
 from oabutton.apps.bookmarklet.models import Event
-import datetime
-
 from test_mongoauth import MongoAuthTest
+import datetime
+import json
 
 class APITest(TestCase):
     def test_add_post(self):
@@ -48,4 +49,29 @@ class APITest(TestCase):
         'email': None}
         for k, v in expected.items():
             assert getattr(evt, k) == v
+
+    def test_event_json(self):
+
+        POST_DATA = {u'story': [u'some access requirement'],
+        u'doi': [u'10.1016/j.urology.2010.05.009.'],
+        u'name': [u'Victor Ng'],
+        u'url': [u'http://www.ncbi.nlm.nih.gov/pubmed/20709373'],
+        u'remember': [u'on'],
+        u'profession': [u'engineer'],
+        u'coords': [u'44,-79.5'],
+        u'location': [u''],
+        u'accessed': [u'Mon, 09 Sep 2013 14:54:42 GMT'],
+        u'description': [u'some description']}
+
+        c = Client()
+        response = c.post('/api/post/', POST_DATA)
+
+        assert response.status_code == 200
+
+        json_data = Event.objects.filter(id=response.context['oid']).to_json()
+        jdata = json.loads(json_data)
+        eq_(len(jdata), 1)
+        eq_(jdata[0]['coords'], {'lat': 44.0, 'lng': -79.5})
+        eq_(jdata[0]['doi'], '10.1016/j.urology.2010.05.009.')
+        eq_(jdata[0]['url'], 'http://www.ncbi.nlm.nih.gov/pubmed/20709373')
 
