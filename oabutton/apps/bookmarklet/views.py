73c2cd1345d431c95025007b3d8b69e94399507c
Victor Ng <victor@crankycoder.com>
2013-10-18 13:15:35 -0400

    switched bookmarklet to a proper django form

--- oabutton/apps/bookmarklet/views.py
+++ oabutton/apps/bookmarklet/views.py
@@ -1,15 +1,15 @@
-from django.http import HttpResponse, HttpResponseServerError
-from django.core.context_processors import csrf
-from django.shortcuts import render_to_response
-from models import Event
-from django.views.decorators.csrf import csrf_protect
 from datetime import datetime
-from oabutton.common import SigninForm
-
 from django.contrib.auth import get_user_model
+from django.core import serializers
+from django.core.context_processors import csrf
+from django.http import HttpResponse, HttpResponseServerError
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
+from models import Event
 from oabutton.apps.bookmarklet.models import User
-
+from oabutton.common import SigninForm, Bookmarklet
 import json
+
 
 def show_map(req):
     # TODO: we need to make this smarter.  Coallescing the lat/long
@@ -19,6 +19,7 @@ def show_map(req):
     count = Event.objects.count()
     context = {'title': 'Map', 'events': json_data, 'count': count}
     return render_to_response(req, 'bookmarklet/site/map.html', context)
+
 
 def get_json(req):
     # Dump all data as JSON.  This seems like a terrible idea when the
@@ -33,12 +34,13 @@ def signin(request):
     One time signin to create a bookmarklet using HTTP POST.
 
     The only required field is the email address
-    
+
     Create a new user and return the URL to the user bookmarklet
     """
-    if request.method == 'POST': # If the form has been submitted...
-        form = SigninForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    if request.method == 'POST':
+        # If the form has been submitted...
+        form = SigninForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
             # TODO: do stuff here
             manager = get_user_model()._default_manager
             data = dict(form.cleaned_data)
@@ -74,9 +76,18 @@ def add(req):
     if 'doi' in req.GET:
         doi = req.GET['doi']
 
-    c.update({'url': url, 'doi': doi})
+    form = Bookmarklet(req.GET)
+    if url:
+        # Add readonly and value attributes to the widget
+        form.fields['url'].widget.attrs['readonly'] = 'readonly'
+        form.fields['url'].widget.attrs['value'] = url
+    if doi:
+        form.fields['doi'].widget.attrs['readonly'] = 'readonly'
+        form.fields['doi'].widget.attrs['value'] = doi
+    c.update({'url': url, 'doi': doi, 'bookmarklet': form})
 
     return render_to_response('bookmarklet/index.html', c)
+
 
 def add_post(req):
     evt_dict = {}
@@ -102,6 +113,8 @@ def add_post(req):
             'doi']
     return render_to_response('bookmarklet/success.html', {'scholar_url': scholar_url, 'oid': str(event.id)})
 
-def generate_bookmarklet(req, user_id): 
+
+def generate_bookmarklet(req, user_id):
     return render_to_response('bookmarklet/bookmarklet.html',
-            {'user_id': user_id}, content_type="application/javascript")
+                              {'user_id': user_id},
+                              content_type="application/javascript")
