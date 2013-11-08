d838b29758f0569bdaab99c56cbebb4184e047b8
Victor Ng <victor@crankycoder.com>
2013-11-08 11:06:56 -0500

    re-applied "Modal Bootstrap 3 signup form"
    This reverts commit b8e9cfee441c3a060b0169327aa27fe16d48023f.
    
    Conflicts:
    	oabutton/apps/web/templates/web/index.jade
    	oabutton/static/public/js/index.js

--- oabutton/static/public/js/index.js
+++ oabutton/static/public/js/index.js
@@ -1,21 +1,17 @@
-// Highlight some node. This ought to be moved to some common JS
-$.fn.animateHighlight = function(highlightColor, duration) {
-    var highlightBg = highlightColor || "#FFFF9C";
-    var animateMs = duration || 1500;
-    var originalBg = this.css("backgroundColor");
-    this.stop().css("background-color",
-    highlightBg).animate({backgroundColor:
-        originalBg}, animateMs);
-};
+$(document).ready(function() {
 
+    // Highlight some node. This ought to be moved to some common JS
+    $.fn.animateHighlight = function(highlightColor, duration) {
+        var highlightBg = highlightColor || "#FFFF9C";
+        var animateMs = duration || 1500;
+        var originalBg = this.css("backgroundColor");
+        this.stop().css("background-color",
+        highlightBg).animate({backgroundColor:
+            originalBg}, animateMs);
+    };
 
-// wait for the DOM to be loaded 
-$(document).ready(function() { 
-    // first hide the bookmarklet
-    // $('#bookmarklet').hide();
-
-    // bind 'myForm' and provide a simple callback function 
-    var options = { 
+    // Bind submission form
+    $('#form-bookmarklet').ajaxForm({ 
         target:     '#divToUpdate',
         dataType:   'json',
         url:        '/api/signin/',
@@ -34,13 +30,29 @@ $(document).ready(function() {
                 $('label.confirm-label').animateHighlight("#dd0000", 1000);
             }
         },
-        success:    function(responseJSON, statusText, xhr, formElem) { 
-            $('#bookmarklet-js').attr('href',
-            "javascript:document.getElementsByTagName('body')[0].appendChild(document.createElement('script')).setAttribute('src','"+responseJSON['url']+"');");
+        success:    function(responseJSON, statusText, xhr, formElem) {
+
+            var bookmarklet = $('#bookmarklet .content');
+            var dialog = $('#bookmarklet-modal');
+
+            // Set URL from service response
+            $('.btn-primary', bookmarklet).attr('href', 
+                "javascript:document.getElementsByTagName('body')[0]" +
+                ".appendChild(document.createElement('script'))" +
+                ".setAttribute('src','"+responseJSON['url']+"');"
+            );
+
+            // Open the modal dialog
+            $('.modal-body', dialog).empty().append(bookmarklet.html());
+            dialog.modal();
 
             // Show the new bookmarklet
             $('#bookmarklet').show();
-        } 
-    }; 
-    $('#form-bookmarklet').ajaxForm(options);
+
+            // Roll up the form
+            $('#form-bookmarklet .form-body').slideUp();
+
+        } // -success
+    }); // -ajaxForm
+
 }); 
