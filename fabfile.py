c499970888239804dc9958c8092b45d873bc1d2f
Victor Ng <victor@crankycoder.com>
2013-11-15 21:57:43 -0500

    added a deploy target in fabric

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,15 @@
 import fabric
-from fabric.api import local
+from fabric.api import local, settings, cd, run
 
 def prepare_deploy():
     local("pip install -r requirements.txt")
     local("make test")
     local("git add -p && git commit")
     local("git push")
+
+def deploy():
+    code_dir = '/home/ubuntu/dev/OAButton/'
+    with settings(warn_only=True):
+        with cd(code_dir):
+            run("git pull")
+            run("sudo supervisorctl restart oabutton")
