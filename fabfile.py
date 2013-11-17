43ec73c8e7a54fc125c5abe7f9ab47cb16693590
Victor Ng <victor@crankycoder.com>
2013-11-17 00:29:28 -0500

    flake8 fixes to fabfile switch to develop before we do a pull

--- fabfile.py
+++ fabfile.py
@@ -15,8 +15,8 @@ Add your SSH key :
     $ fab -H ubuntu@staging.openaccessbutton.org deploy
 
 """
-import fabric
 from fabric.api import local, settings, cd, run
+
 
 def prepare_deploy():
     local("pip install -r requirements.txt")
@@ -24,10 +24,12 @@ def prepare_deploy():
     local("git add -p && git commit")
     local("git push")
 
+
 def deploy():
     code_dir = '/home/ubuntu/dev/OAButton/'
     with settings(warn_only=True):
         with cd(code_dir):
+            run("git branch develop")
             run("git pull")
             run("pip install -r requirements.txt")
             run("sudo supervisorctl restart oabutton")
