8d8ddf7ad4d540a892fd0b1a5784038a0662da53
Victor Ng <victor@crankycoder.com>
2014-03-07 15:35:58 -0500

    more refactoring of email address extractor

--- oabutton/phantomjs/grab_page.py
+++ oabutton/phantomjs/grab_page.py
@@ -1,8 +1,20 @@
 import subprocess
 import re
+import tempfile
 
-command = r"""phantomjs loadit.example.js"""
-child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdoutdata, stderrdata = child.communicate()
-possible_emails = set([f[0] for f in re.findall(r"(\w+@\w+(.\w+))", stdoutdata)])
-print "Found emails: ", possible_emails
+
+def grab_emails(url):
+    data = open('loadit_template.js', 'r').read()
+    script = data.replace("%URL%", "http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract")
+    with tempfile.NamedTemporaryFile(suffix='.js', delete=True) as tmpfile:
+        tmpfile.write(script)
+        tmpfile.flush()
+        command = 'phantomjs %s' % tmpfile.name
+        child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdoutdata, stderrdata = child.communicate()
+        possible_emails = set([f[0] for f in re.findall(r"(\w+@\w+(\.\w+))", stdoutdata)])
+        return possible_emails
+    return set()
+
+
+print "Found emails: ", grab_emails("http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract")
--- oabutton/phantomjs/runme.js
+++ oabutton/phantomjs/runme.js
@@ -1,7 +1,13 @@
 
 var page = require('webpage').create();
 var url = "http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract";
-//var url = system.args[1];
+page.settings.resourceTimeout = 5000; // 5 seconds
+page.onResourceTimeout = function(e) {
+    console.log(e.errorCode);   // it'll probably be 408 
+    console.log(e.errorString); // it'll probably be 'Network timeout on resource'
+    console.log(e.url);         // the url whose request timed out
+    phantom.exit(1);
+};
 
 page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
 page.open(url, function (status) {
