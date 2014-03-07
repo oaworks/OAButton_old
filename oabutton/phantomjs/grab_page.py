ec883c0283bba417904df3a87e4e9bf4ed605023
Victor Ng <victor@crankycoder.com>
2014-03-07 15:42:19 -0500

    got rid of all template js

--- oabutton/phantomjs/grab_page.py
+++ oabutton/phantomjs/grab_page.py
@@ -3,9 +3,32 @@ import re
 import tempfile
 
 
+js_template = r"""
+var page = require('webpage').create();
+var url = "%URL%";
+page.settings.resourceTimeout = 5000; // 5 seconds
+page.onResourceTimeout = function(e) {
+    console.log(e.errorCode);   // it'll probably be 408
+    console.log(e.errorString); // it'll probably be 'Network timeout on resource'
+    console.log(e.url);         // the url whose request timed out
+    phantom.exit(1);
+};
+
+page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
+page.open(url, function (status) {
+    page.evaluate(function () {
+        return document.title;
+    }, function(result){
+
+    });
+    console.log(page.content);
+    phantom.exit();
+});
+"""
+
+
 def grab_emails(url):
-    data = open('loadit_template.js', 'r').read()
-    script = data.replace("%URL%", "http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract")
+    script = js_template.replace("%URL%", url)
     with tempfile.NamedTemporaryFile(suffix='.js', delete=True) as tmpfile:
         tmpfile.write(script)
         tmpfile.flush()
@@ -15,6 +38,3 @@ def grab_emails(url):
         possible_emails = set([f[0] for f in re.findall(r"(\w+@\w+(\.\w+))", stdoutdata)])
         return possible_emails
     return set()
-
-
-print "Found emails: ", grab_emails("http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract")
