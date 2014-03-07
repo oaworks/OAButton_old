4f58be28d423f088b01e772fb0d045ff28380ccb
Victor Ng <victor@crankycoder.com>
2014-03-07 11:17:25 -0500

    * changed static load settings so that things work in dev properly * moved non-SSL content onto local server

--- oabutton/settings.py
+++ oabutton/settings.py
@@ -84,21 +84,23 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = STATIC_PUBLIC
+
+# STATIC_ROOT is only used by collectstatic
+#STATIC_ROOT = STATIC_PUBLIC
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = (STATIC_PUBLIC,)
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # We don't use any regular security bits, so just make up some random
