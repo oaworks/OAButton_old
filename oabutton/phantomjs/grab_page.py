1797bb207911776038648c12b89ae6f7e61727c0
Victor Ng <victor@crankycoder.com>
2014-03-07 15:00:51 -0500

    refactored URL into python code for email extraction

--- oabutton/phantomjs/grab_page.py
+++ oabutton/phantomjs/grab_page.py
@@ -1,7 +1,7 @@
 import subprocess
 import re
 
-command = "phantomjs loadit.example.js"
+command = r"""phantomjs loadit.example.js http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract"""
 child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 stdoutdata, stderrdata = child.communicate()
 possible_emails = set([f[0] for f in re.findall(r"(\w+@\w+(.\w+))", stdoutdata)])
--- oabutton/phantomjs/loadit.example.js
+++ oabutton/phantomjs/loadit.example.js
@@ -1,5 +1,6 @@
 var page = require('webpage').create();
-var url = 'http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract';
+var url = system.args[1];
+
 page.open(url, function (status) {
     page.evaluate(function () {
         return document.title;
