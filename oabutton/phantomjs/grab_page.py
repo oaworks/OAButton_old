33582bdedea06b9c63f11d290270c1ed8d059c03
Victor Ng <victor@crankycoder.com>
2014-03-07 15:14:36 -0500

    capture stderr so that we don't spew to console

--- oabutton/phantomjs/grab_page.py
+++ oabutton/phantomjs/grab_page.py
@@ -1,8 +1,8 @@
 import subprocess
 import re
 
-command = r"""phantomjs loadit.example.js http://stke.sciencemag.org/cgi/content/abstract/sigtrans;6/302/ra100?view=abstract"""
-child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+command = r"""phantomjs loadit.example.js"""
+child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdoutdata, stderrdata = child.communicate()
 possible_emails = set([f[0] for f in re.findall(r"(\w+@\w+(.\w+))", stdoutdata)])
 print "Found emails: ", possible_emails
--- oabutton/phantomjs/loadit.js
+++ oabutton/phantomjs/loadit.js
@@ -1,5 +1,6 @@
 var page = require('webpage').create();
 var url = 'URL_GOES_HERE';
+page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
 page.open(url, function (status) {
     page.evaluate(function () {
         return document.title;
